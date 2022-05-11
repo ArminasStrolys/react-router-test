463e42ae1c518196630217d228f6f70bacf2b02d
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-11 15:25:58 +0300

    Router test

---
+++ src/modules/app/App.js
@@ -0,0 +1,13 @@
+import './app.css'
+import Nav from '../nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+    <Nav />
+      <p>test</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./nav.css";
+
+const Nav = () => {
+  return (
+    <div className="main">
+      <div className="navbar">
+        <ul className="uList">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Pictures</a>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
